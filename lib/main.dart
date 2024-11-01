f6dcb3b44bb3c56373d82059d14e4742f1d3bc2c
BrunoPascoa76 <brunopascoa03@ua.pt>
2024-11-01 15:15:00 +0000

    AN-15 did main setup to change app based on theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:anitrack/service/anilist_auth.dart';
 import 'package:anitrack/ui/navbar.dart';
+import 'package:anitrack/utils/theme_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
 
 final getIt=GetIt.instance;
@@ -9,6 +13,9 @@ final getIt=GetIt.instance;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   getIt.registerLazySingleton(()=>AnilistAuth());
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: await getTemporaryDirectory(),
+  );
   runApp(const MainApp());
 }
 
@@ -17,9 +24,19 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      home: const Navbar()
+    return BlocProvider(
+      create: (context)=>ThemeCubit(), 
+      child: BlocBuilder<ThemeCubit,ThemeController>(
+        builder: (context,themeController) {
+          AppTheme theme=themeController.themes[themeController.currentTheme];
+          return MaterialApp(
+            theme: theme.ligthTheme,
+            darkTheme: theme.darkTheme,
+            themeMode: themeController.mode,
+            home: const Navbar()
+          );
+        }
+      ),
     );
   }
 }
