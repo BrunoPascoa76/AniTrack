0eb8e0d57bd33b03a69739aa1d30c7cecccc55bc
BrunoPascoa76 <brunopascoa03@ua.pt>
2024-11-11 10:43:33 +0000

    temp save to work on ES

--- lib/model/notification.dart
+++ lib/model/notification.dart
@@ -26,5 +26,89 @@ class NotificationQueue{
 }
 
 class Notification {
+  String type;
+  int createdAt;
+
+  Notification({required this.type,required this.createdAt});
+
+  factory Notification.fromJson(Map<String,dynamic> json){
+    switch(json["type"]){
+      case "AIRING":
+        return AiringNotification.fromJson(json);
+      case "RELATED_MEDIA_ADDITION":
+        return RelatedMediaAdditionNotification.fromJson(json);
+      case "MEDIA_DATA_CHANGE":
+        return MediaDataChangeNotification.fromJson(json);
+      case "MEDIA_MERGE":
+        return MediaMergeNotification.fromJson(json);
+      case "MEDIA_DELETION":
+        return MediaDeleteNotification.fromJson(json);
+      default:
+        throw Exception("Unknown notification type: ${json["type"]}");
+    }
+  }
+
+  Map<String,dynamic> toJson(){
+    return {
+      "type":type,
+      "createdAt":createdAt,
+    };
+  }
+}
+
+class AiringNotification extends Notification{
+  int animeId;
+  int episode;
+
+  AiringNotification({super.type="AIRING",required super.createdAt,required this.animeId,required this.episode});
   
+  factory AiringNotification.fromJson(Map<String,dynamic> json){
+    return AiringNotification(
+      createdAt: json["createdAt"] as int,
+      animeId: json["animeId"] as int,
+      episode: json["episode"] as int,
+    );
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      ...super.toJson(),
+      "animeId":animeId,
+      "episode":episode
+    };
+  }
+}
+
+class RelatedMediaAdditionNotification extends Notification{
+  int mediaId;
+
+  RelatedMediaAdditionNotification({super.type="RELATED_MEDIA_ADDITION",required super.createdAt,required this.mediaId});
+
+  factory RelatedMediaAdditionNotification.fromJson(Map<String,dynamic> json){
+    return RelatedMediaAdditionNotification(
+      createdAt: json["createdAt"] as int,
+      mediaId: json["mediaId"] as int
+    );
+  }
+
+  @override
+  Map<String, dynamic> toJson(){
+    return {
+      ...super.toJson(),
+      "mediaId":mediaId
+    };
+  }
+}
+
+class MediaDataChangeNotification extends Notification{
+  
+}
+
+class MediaMergeNotification extends Notification{
+
+}
+
+class MediaDeleteNotification extends Notification{
+
 }
